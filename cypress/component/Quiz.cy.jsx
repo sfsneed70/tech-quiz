5ae0af20c128439c2e6a3d37990f9aa291466596
avca1998 <sfsneed@gmail.com>
2024-11-05 11:42:33 -0800

    component testing

--- cypress/component/Quiz.cy.jsx
+++ cypress/component/Quiz.cy.jsx
@@ -18,13 +18,21 @@ describe("Quiz", () => {
       cy.get(".btn").should("have.text", "Start Quiz");
     });
 
-    it("should render the first Quiz question card", () => {
+    // it("should render the first Quiz question card", () => {
+    //   cy.get(".btn").should("have.text", "Start Quiz").click();
+    //   cy.wait('@getQuestions');
+    //   cy.get(".card").should("be.visible");
+    // });
+
+    it("verify contents of first question", () => {
       cy.get(".btn").should("have.text", "Start Quiz").click();
-      cy.get(".card").should("be.visible");
+      cy.wait('@getQuestions');
+      cy.get(".card h2").should("have.text", "What is the output of print(2 ** 3)?");
     });
 
     it("should complete quiz and show results", () => {
       cy.get(".btn").should("have.text", "Start Quiz").click();
+      cy.wait('@getQuestions');
       // click answer 2 for all 10 questions
       for (let i = 0; i < 10; i++) {
         cy.get(".btn").contains("2").click();
@@ -35,6 +43,7 @@ describe("Quiz", () => {
 
     it("clicking take new quiz should start new quiz", () => {
       cy.get(".btn").should("have.text", "Start Quiz").click();
+      cy.wait('@getQuestions');
       // click answer 2 for all 10 questions
       for (let i = 0; i < 10; i++) {
         cy.get(".btn").contains("1").click();
